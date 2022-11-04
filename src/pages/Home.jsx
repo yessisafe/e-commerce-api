6b12c1912fea67d62812f4f9e637a8ac9773c37c
Yessica <yessisafe@gmail.com>
2022-11-04 00:40:11 -0600

    Agregando navbar y componentes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import Header from "../components/Header";
+import Header from "../components/Header/Header"
 
 const Home = () => {
   const [items, setItems] = useState([]);
